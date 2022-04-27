1a45a9cb667ab72fbd21a023b55a17ddc2486e83
hjpotter92 <git@hjpotter92.email>
2022-04-27 15:14:40 +0530

    cask: Create generic go-livepeer cask

---
+++ Casks/go-livepeer.rb
@@ -0,0 +1,32 @@
+cask "go-livepeer" do
+  arch = Hardware::CPU.intel? ? "amd64" : "arm64"
+  platform = OS.mac? ? "darwin" : "linux"
+  version "0.5.29"
+
+  url "https://github.com/livepeer/go-livepeer/releases/download/v#{version}/livepeer-#{platform}-#{arch}.tar.gz"
+  name "go-livepeer"
+  desc "Official Go implementation of the Livepeer protocol"
+  homepage "https://livepeer.org/"
+
+  if arch == "amd64" && OS.mac?
+    sha256 "a342177e47e72bee030533271d69c1316cccf5784e6cf5638a5452bd3021eaeb"
+  elsif arch == "amd64" && OS.linux?
+    sha256 "276936a0d3d3dea9fbacfc464ee1c12deddc4262c20427e0c3f9a2cfeeb1a6ac"
+  elsif arch == "arm64"
+    sha256 "1cf2b2c606d18e4c515256b8b364777312b62dffa92075557adde0d9265feec2"
+  end
+
+  livecheck do
+    url "https://github.com/livepeer/go-livepeer/releases/latest/"
+    strategy :header_match do |headers|
+      id = headers["location"][/(\d+\.\d+\.\d+)$/, 1]
+
+      id.to_s
+    end
+  end
+
+  binary "livepeer"
+  binary "livepeer_bench"
+  binary "livepeer_cli"
+  binary "livepeer_router"
+end
