3cc0f7f617df1364ff53fb13a2b2eafed6c86dbf
hjpotter92 <hjpotter92@users.noreply.github.com>
2022-05-25 14:32:06 +0530

    go-livepeer: Update to new stable release of go-livepeer (#5)

--- Casks/go-livepeer.rb
+++ Casks/go-livepeer.rb
@@ -1,14 +1,14 @@
 cask "go-livepeer" do
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
   platform = OS.mac? ? "darwin" : "linux"
-  version "0.5.29"
+  version "0.5.31"
 
   if arch == "amd64" && OS.mac?
-    sha256 "a342177e47e72bee030533271d69c1316cccf5784e6cf5638a5452bd3021eaeb"
+    sha256 "bd018ba81c91d76c6525546e242ce672187cc168aa34f80b9d66670a7418ee5b"
   elsif arch == "amd64" && OS.linux?
-    sha256 "276936a0d3d3dea9fbacfc464ee1c12deddc4262c20427e0c3f9a2cfeeb1a6ac"
+    sha256 "40e605422b0a7d0f26275d9c8e0de02fc4b1005844259c36f84ca0a53bf19f3e"
   elsif arch == "arm64"
-    sha256 "1cf2b2c606d18e4c515256b8b364777312b62dffa92075557adde0d9265feec2"
+    sha256 "119ee9ffd4a92084b29ca1b264301817b627e236ac947e4d78385ff21a3e92fc"
   end
 
   url "https://github.com/livepeer/go-livepeer/releases/download/v#{version}/livepeer-#{platform}-#{arch}.tar.gz",
