a600608c926ff488cf30f2e66d148ec256c13b5a
hjpotter92 <git@hjpotter92.email>
2022-08-11 10:25:38 +0530

    cask: Reorganise cask definition.
    Add `test` stanza to go-livepeer formula.

--- Casks/go-livepeer.rb
+++ Casks/go-livepeer.rb
@@ -1,14 +1,28 @@
 cask "go-livepeer" do
-  arch = Hardware::CPU.intel? ? "amd64" : "arm64"
-  platform = OS.mac? ? "darwin" : "linux"
   version "0.5.33"
 
-  if arch == "amd64" && OS.mac?
-    sha256 "b3c045f0d0764247d299e4c3f94fdb01cbdf65fec059853e4ec0426d9059982f"
-  elsif arch == "amd64" && OS.linux?
-    sha256 "93207cba5d5ea15cb0e9396811d15bcf039b351db53bef522626fd42d0afd877"
-  elsif arch == "arm64"
-    sha256 "0f4f486c30b48bc86355c36af26db06a74fd8dd4ca2264360bbdcdf0d5692987"
+  license "MIT"
+
+  if OS.mac?
+    platform = "darwin"
+    if Hardware::CPU.intel?
+      sha256 "b3c045f0d0764247d299e4c3f94fdb01cbdf65fec059853e4ec0426d9059982f"
+      arch = "amd64"
+    elsif Hardware::CPU.arm?
+      sha256 "0f4f486c30b48bc86355c36af26db06a74fd8dd4ca2264360bbdcdf0d5692987"
+      arch = "arm64"
+    end
+  end
+
+  if OS.linux?
+    platform = "linux"
+    if Hardware::CPU.intel?
+      sha256 "93207cba5d5ea15cb0e9396811d15bcf039b351db53bef522626fd42d0afd877"
+      arch = "amd64"
+    elsif Hardware::CPU.arm?
+      sha256 "7d062d106272bbf1645a2b5d8a276a1a5944fdbbb530b0def1c9c3b5d2d247b4"
+      arch = "arm64"
+    end
   end
 
   url "https://github.com/livepeer/go-livepeer/releases/download/v#{version}/livepeer-#{platform}-#{arch}.tar.gz",
@@ -16,6 +30,8 @@ cask "go-livepeer" do
   name "go-livepeer"
   desc "Official Go implementation of the Livepeer protocol"
   homepage "https://livepeer.org/"
+
+  container_type :gzip
 
   livecheck do
     url "https://github.com/livepeer/go-livepeer/releases/latest/"
