a6cc404c342a22e97d2c5a373a25a6718d40f05b
Thom Shutt <thomshutt@users.noreply.github.com>
2022-08-16 17:12:03 +0100

    go-livepeer: Update to version 0.5.34 (#9)

--- Casks/go-livepeer.rb
+++ Casks/go-livepeer.rb
@@ -1,15 +1,15 @@
 cask "go-livepeer" do
-  version "0.5.33"
+  version "0.5.34"
 
   license "MIT"
 
   if OS.mac?
     platform = "darwin"
     if Hardware::CPU.intel?
-      sha256 "b3c045f0d0764247d299e4c3f94fdb01cbdf65fec059853e4ec0426d9059982f"
+      sha256 "769a6a86c7ca7d507257f66fee90a660fd39378a09bd654c22161bb97e8b1dd9"
       arch = "amd64"
     elsif Hardware::CPU.arm?
-      sha256 "0f4f486c30b48bc86355c36af26db06a74fd8dd4ca2264360bbdcdf0d5692987"
+      sha256 "54dadfa57a9d5bdbed761763ad60c4cbd60802c4add72df4e59b8f740925f021"
       arch = "arm64"
     end
   end
@@ -17,10 +17,10 @@ cask "go-livepeer" do
   if OS.linux?
     platform = "linux"
     if Hardware::CPU.intel?
-      sha256 "93207cba5d5ea15cb0e9396811d15bcf039b351db53bef522626fd42d0afd877"
+      sha256 "1a53c8ee0be0c93b9f1f6a2458ce469401b7623e577a3c22708446c0c21df5a9"
       arch = "amd64"
     elsif Hardware::CPU.arm?
-      sha256 "7d062d106272bbf1645a2b5d8a276a1a5944fdbbb530b0def1c9c3b5d2d247b4"
+      sha256 "fb27c59063d14e686f844c08cbe28a44519e475b3b18c3901411d206ba446c2a"
       arch = "arm64"
     end
   end
